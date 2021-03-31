5eca4b0a3c7054a7b8fcda07427f247e4e4273d8
王庆炜 <45898800+gggllhha@users.noreply.github.com>
2021-03-31 16:04:51 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,57 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
-void MergeStep(int a[], int r[], int s, int m, int n) {       //¹é²¢º¯Êý
-	int i, j, k;
-	k = i = s;
-	j = m + 1;
-	while (i <= m && j <= n) {   //Á½¸ö±í¶¼Ã»½áÊøÊ±Ñ­»·±È½Ï
-		if (a[i] <= a[j])        //µ±½ÏÐ¡µÄÔªËØ¸´ÖÆµ½RÖÐ
+#define MAXVALUE 32
+void MergeStep(int a[], int r[], int low, int mid, int high) {       //å½’å¹¶å‡½æ•°
+	int i = low;
+	int j = mid + 1;
+	int m = mid;
+	int n = high;
+	int k = 0;
+	while (i <= m && j <= n) {   //ä¸¤ä¸ªè¡¨éƒ½æ²¡ç»“æŸæ—¶å¾ªçŽ¯æ¯”è¾ƒ
+		if (a[i] <= a[j])        //å½“è¾ƒå°çš„å…ƒç´ å¤åˆ¶åˆ°Rä¸­
 			r[k++] = a[i++];
 		else
 			r[k++] = a[j++];
 	}
-	while (i <= m)            //½«Î´ºÏ²¢µÄ¸´ÖÆµ½RÖÐ
+	while (i <= m)            
 		r[k++] = a[i++];
 	while (j <= n)
 		r[k++] = a[j++];
+
+	for (i = 0; i < k; i++)
+		a[low + i] = r[i];
 }
-void MergePass(int a[], int r[], int n, int len) {   //Ò»±éºÏ²¢
-	int s, e;
-	s = 0;
-	while (s + len < n) {
-		e = s + 2 * len - 1;
-		MergeStep(a, r, s, s + len - 1, e);
-		s = e + 1;
-	}
-	if (s < n) {
-		for (; s < n; s++)
-			r[s] = a[s];
-	}
-}
-void MergeSort(int a[], int n) {    
-	int* p;
-	int len = 1;
-	int f = 0;
-	if (!(p = (int*)malloc(sizeof(int) * n))) {
-		printf("·ÖÅäÁÙÊ±ÄÚ´æÊ§°Ü£¡\n");
-		exit(0);
-	}
-	while (len < n) {
-		if (f)
-			MergePass(p, a, n, len);
-		else
-			MergePass(a, p, n, len);
-		len *= 2;
-		f = 1 - f;
-	}
-	if (f) {
-		for (f = 0; f < n; f++) {
-			a[f] = p[f];
-		}
-		free(p);
+void MergeSort(int a[],int r[], int low,int high){    
+	if (low < high) {
+		int mid = (low + high) / 2;
+		MergeSort(a, r, low,mid);
+		MergeSort(a, r, mid+1, high);
+		MergeStep(a, r,low, mid, high);
 	}
 }
 int main(void) {
-
+	int A[8] = { 1,5,8,6,2,4,3,7 };
+	int* r;
+	r = (int*)malloc(sizeof(int) * MAXVALUE);
+	MergeSort(A,r,0,7);
+	free(r);
+	for (int i = 0; i < 8; i++) {
+		printf("%d", A[i]);
+	}
 	system("pause");
 }
